5a97c8c20fd14c8ee3967c0eaef0ee6665215477
John Fazakerley <johnfaz1994@hotmail.com>
2018-11-01 10:54:58 +1030

    Creation of Project

---
+++ Production for Clients/Assets/Scripts/Canvas Manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour
+{
+    public GameObject currentCanvas;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //need to set canvas most recently enabled as "current canvas" for SwitchCanvas script, to avoid hard coding 'change month' buttons
+    }
+}
---
+++ Production for Clients/Assets/Scripts/SwitchCanvas.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SwitchCanvas : MonoBehaviour {
+
+    public GameObject currentCanvas;
+    public GameObject canvasToSwitchTo;
+
+
+	public void SwapCanvas()
+    {
+        currentCanvas.SetActive(false);
+        canvasToSwitchTo.SetActive(true);
+    }
+}
